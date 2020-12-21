5dcaa8327977b0c02875ef41226d16f79e81604f
WaWaKids <wawakidss@gmail.com>
2020-12-21 09:00:29 +0300

    -Now it works with Retrofit2

--- app/src/main/java/android/wawakidss/test_task/MatchActivity.java
+++ app/src/main/java/android/wawakidss/test_task/MatchActivity.java
@@ -2,32 +2,33 @@ package android.wawakidss.test_task;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.wawakidss.test_task.data.request.tournament.MatchParams;
+import android.wawakidss.test_task.data.request.tournament.MatchRequest;
+import android.wawakidss.test_task.data.request.video.VideoRequest;
+import android.wawakidss.test_task.data.response.tournament.MatchResponse;
+import android.wawakidss.test_task.data.response.video.VideoResponse;
+import android.wawakidss.test_task.retrofit.InstatAPI;
+import android.wawakidss.test_task.retrofit.RetrofitClient;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONStringer;
-import org.w3c.dom.Text;
 
-import java.io.IOException;
+import java.util.List;
 
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MatchActivity extends AppCompatActivity {
 
     private static final String TAG = "MatchActivity";
     private static final String MATCH_URL = "http://api.instat.tv/test/data";
     private static final String VIDEOS_URL = "https://api.instat.tv/test/video-urls";
+    private InstatAPI apiInterface = RetrofitClient.getClient().create(InstatAPI.class);
     private TextView text;
-    public static final MediaType JSON
-            = MediaType.get("application/json; charset=utf-8");
-    OkHttpClient client = new OkHttpClient();
 
 
     @Override
@@ -35,44 +36,46 @@ public class MatchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
 
-
         Bundle args = getIntent().getExtras();
-        int _p_sport = args.getInt("_p_sport");
-        int _p_match_id = args.getInt("_p_match_id");
+        int sport = args.getInt("_p_sport");
+        int matchId = args.getInt("_p_match_id");
 
-        JSONObject jsonMatchParams = new JSONObject();
-        try {
-            jsonMatchParams.put("_p_sport", _p_sport);
-            jsonMatchParams.put("_p_match_id", _p_match_id);
-            Log.d(TAG, "jsonMatchParams: " + jsonMatchParams.toString());
-        } catch (JSONException e) {
-            e.printStackTrace();
-            Log.d(TAG, "failed to parse json");
-        }
+        MatchRequest matchRequest = new MatchRequest("get_match_info", sport, matchId);
 
-        Log.d(TAG, jsonMatchParams.toString());
-
-        final JSONObject jsonObject = new JSONObject();
-        try {
-            jsonObject.put("proc", "get_match_info");
-            jsonObject.put("params", jsonMatchParams);
-            Log.d(TAG, "jsonObject: " + jsonObject.toString());
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        String matchResponse = post(MATCH_URL, jsonObject.toString());
-        Log.d(TAG, "response: " + matchResponse);
+        Call<MatchResponse> matchResponseCall = apiInterface.getMatchData(matchRequest);
+        sendMatchPost(matchRequest);
+        VideoRequest videoRequest = new VideoRequest(matchId, sport);
     }
 
-    String post(String url, String json) throws IOException {
-        RequestBody body = RequestBody.create(json, JSON);
-        Request request = new Request.Builder()
-                .url(url)
-                .post(body)
-                .build();
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
-        }
+    public void sendMatchPost(MatchRequest matchRequest) {
+        apiInterface.getMatchData(matchRequest).enqueue(new Callback<MatchResponse>() {
+            @Override
+            public void onResponse(Call<MatchResponse> call, Response<MatchResponse> response) {
+                if (response.isSuccessful()) {
+                    Log.d(TAG, "post submitted to API." + response.body().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MatchResponse> call, Throwable t) {
+                Log.d(TAG, "Unable to submit match post to API");
+            }
+        });
+    }
+
+    public void sendVideoPost(VideoRequest videoRequest) {
+        apiInterface.getVideos(videoRequest).enqueue(new Callback<List<VideoResponse>>() {
+            @Override
+            public void onResponse(Call<List<VideoResponse>> call, Response<List<VideoResponse>> response) {
+                if (response.isSuccessful()) {
+                    Log.d(TAG, "post submitted to API." + response.body());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<VideoResponse>> call, Throwable t) {
+                Log.d(TAG, "Unable to submit video post to API");
+            }
+        });
     }
 }
