a83fc32f7a042c4572d1d1f6a9b2d4e601047cf6
WaWaKids <wawakidss@gmail.com>
2020-12-23 06:14:38 +0300

    -Changed method with videos so now it receives 1 body argument instead of 2 fields

--- app/src/main/java/android/wawakidss/test_task/retrofit/InstatAPI.java
+++ app/src/main/java/android/wawakidss/test_task/retrofit/InstatAPI.java
@@ -1,9 +1,9 @@
 package android.wawakidss.test_task.retrofit;
 
 import android.wawakidss.test_task.data.MatchRequest;
-import android.wawakidss.test_task.data.VideoRequest;
 import android.wawakidss.test_task.data.MatchResponse;
-import android.wawakidss.test_task.data.VideoResponse;
+import android.wawakidss.test_task.data.VideosRequest;
+import android.wawakidss.test_task.data.VideosResponse;
 
 import java.util.List;
 
@@ -16,11 +16,10 @@ import retrofit2.http.POST;
 public interface InstatAPI {
 
     @POST("test/data")
-    @Headers({ "Content-Type: application/json"})
+    @Headers({"Content-Type: application/json"})
     Call<MatchResponse> getMatchData(@Body MatchRequest matchRequest);
 
     @POST("test/videoResponse-urls")
-    @Headers({ "Content-Type: application/json"})
-    Call<List<VideoResponse>> getVideos(@Field("match_id") int matchId,
-                                        @Field("sport_id") int sportId);
+    @Headers({"Content-Type: application/json"})
+    Call<List<VideosResponse>> getVideos(@Body VideosRequest videosRequest);
 }
